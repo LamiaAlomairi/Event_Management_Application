423d396e6f4c93f5d7c589e89a3e7546327affa2
LamiaAlomairi <lama5alomairi@gmail.com>
2023-05-03 12:16:34 +0400

    venue model attributes

--- Event_Management_Application/src/main/java/com/Event_Management_Application/Event_Management_Application/Models/Venue.java
+++ Event_Management_Application/src/main/java/com/Event_Management_Application/Event_Management_Application/Models/Venue.java
@@ -1,4 +1,23 @@
 package com.Event_Management_Application.Event_Management_Application.Models;
 
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@Data
+@Entity
+@Table(name = "venue")
 public class Venue {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+    String name;
+    String address;
+    String capacity;
+    String email;
+    Integer phone;
 }
