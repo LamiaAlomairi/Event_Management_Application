12a46a4ae77dea2f1ab998b4e6cd9aeae979b939
LamiaAlomairi <lama5alomairi@gmail.com>
2023-05-03 12:25:14 +0400

    Attendee model attributes

--- Event_Management_Application/src/main/java/com/Event_Management_Application/Event_Management_Application/Models/Attendee.java
+++ Event_Management_Application/src/main/java/com/Event_Management_Application/Event_Management_Application/Models/Attendee.java
@@ -1,4 +1,22 @@
 package com.Event_Management_Application.Event_Management_Application.Models;
 
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@Data
+@Entity
+@Table(name = "attendee")
 public class Attendee {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+    String name;
+    String email;
+    Integer phone;
+    String address;
 }
