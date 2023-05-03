8f5390e08bd9f112c7d9f7bba71fbace0c1c73cc
LamiaAlomairi <lama5alomairi@gmail.com>
2023-05-03 12:22:49 +0400

    Ticket model attributes

--- Event_Management_Application/src/main/java/com/Event_Management_Application/Event_Management_Application/Models/Ticket.java
+++ Event_Management_Application/src/main/java/com/Event_Management_Application/Event_Management_Application/Models/Ticket.java
@@ -1,4 +1,24 @@
 package com.Event_Management_Application.Event_Management_Application.Models;
 
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@Data
+@Entity
+@Table(name = "ticket")
 public class Ticket {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+    String type_VIP_general;
+    Double price;
+    Integer quantity;
+    String sale_start_date;
+    String sale_end_date;
+
 }
